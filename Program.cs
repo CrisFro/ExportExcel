91db3b2a721f0e701db9af2dedc17d4f31423ffb
Cristiane Guimarães Fröhlich <cristiane_g_frohlich@hotmail.com>
2022-10-18 16:21:15 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using ClosedXML.Excel;
+
+namespace ExportExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Planilha1");
+                worksheet.Cell("A1").Value = "Olá mundo";
+                worksheet.Cell("A2").Value = 1;
+                worksheet.Cell("A3").Value = 2;
+                worksheet.Cell("A4").Value = 3;
+
+                //utilizando fórmula
+                worksheet.Cell("A5").FormulaA1 = "=SUM(A2:A4)";
+
+                //utilizando imagens
+                var caminhoImagem = @"C:\Users\Cristiane\Desktop\Exercícios\ExportExcel\img\csharp.png";
+                worksheet.AddPicture(caminhoImagem).MoveTo(worksheet.Cell("A10")).Scale(0.5);
+
+                //utilizando bordas
+                worksheet.Cell("A1").Style.Border.BottomBorder = XLBorderStyleValues.Thick;
+                worksheet.Cell("A1").Style.Border.BottomBorderColor = XLColor.Red; 
+                worksheet.Cell("A1").Style.Border.LeftBorder = XLBorderStyleValues.Thick;
+                worksheet.Cell("A1").Style.Border.LeftBorderColor = XLColor.Red;
+                worksheet.Cell("A1").Style.Border.RightBorder = XLBorderStyleValues.Thick;
+                worksheet.Cell("A1").Style.Border.RightBorderColor = XLColor.Red;
+                worksheet.Cell("A1").Style.Border.TopBorder = XLBorderStyleValues.Thick;
+                worksheet.Cell("A1").Style.Border.TopBorderColor = XLColor.Red;
+
+                //Calculando com fórmula
+                Console.WriteLine("Valor da soma: {0}", worksheet.Cell("A5").Value);
+
+                workbook.SaveAs(@"c:\Users\Cristiane\Desktop\Exercícios\exportExcel.xlsx");
+            }
+
+            Process.Start(new ProcessStartInfo(@"C:\Users\Cristiane\Desktop\Exercícios\ExportExcel\exportExcel.xlsx") { UseShellExecute = true });
+        }
+    }
+}
